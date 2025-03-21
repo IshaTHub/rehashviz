682130caa24e9ab1a120825b3ed6f851e04aa7fe
IshaTHub <ishatiwari0508@gmail.com>
2025-03-21 23:57:42 +0530

    Demo Section pizza icon and heading

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <footer className="bg-pink-50 py-12">
+        <footer className="bg-pink-50 py-6">
             Footer
         </footer>
     )
