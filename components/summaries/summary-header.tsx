92d8227b826177c80a246e34b179e45ba8da2350
IshaTHub <ishatiwari0508@gmail.com>
2025-04-07 02:06:47 +0530

    style: :lipstick: Icons for date, reading mins back button link

--- components/summaries/summary-header.tsx
+++ components/summaries/summary-header.tsx
@@ -1,8 +1,62 @@
-export function SummaryHeader({title} : {title: string}){
+import { Button } from "@/components/ui/button";
+import { Calendar, ChevronLeft, Clock, Sparkle } from "lucide-react";
+import  Link  from "next/link";
+import { Badge } from "@/components/ui/badge";
+
+
+export function SummaryHeader({title, createdAt, readingTime} : {title: string, createdAt: string, readingTime: number}){
+
 return(
-    <h1 className="">
-        {title}
-    </h1>
+    <div className="flex gap-4 mb-4 justify-between">
+      <div className="space-y-6">
+<div className="flex flex-wrap items-center gap-4">
+    <Badge
+    variant="secondary"
+
+    className="relative px-4 py-1.5 text-sm
+    
+    font-medium bg-white/80 backdrop-blur-xs
+    
+    rounded-full hover:bg-white/90 transition-all
+    
+    duration-200 shadow-xs hover:shadow-md" >
+        <Sparkle/>
+        AI Summary
+    </Badge>
+    <div className="flex items-center gap-2 text-sm text-muted-foreground">
+        <Calendar className="h-4 w-4 text-red-400"/>
+        {new Date(createdAt).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+    </div>
+    <div className="flex items-center gap-2 text-sm text-muted-foreground">
+        <Clock className="h-4 w-4 text-red-400"/>
+       {readingTime} min read
+    </div>
+</div>
+      </div>
+      <div>
+        <Link href="/dashboard" >
+        <Button variant='link'
+      size="sm" 
+      className="group flex items-center gap-1 sm:gap-2
+      hover:bg-white/80 backdrop-blur-xs rounded-full
+      transition-all duration-200 shadow-xs
+      hover:shadow:md border border-rose-100/30
+      bg-rose-100 px-2 sm:px-3"
+     
+      >
+        <ChevronLeft className="w-4 h-4 sm:w-5 sm:h-5 text-rose-400
+        transition-transform group-hover:translate-x-0.5 " />
+       <span className="text-xs sm:text-sm text-muted-foreground font-medium"> 
+        Back <span className="hidden sm:inline"> To Dashboard </span>
+        </span>
+      </Button>
+      </Link>
+      </div>
+    </div>
 );
 }
 
